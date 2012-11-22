e7761aefcefe87b3c2b36054222aad3c93dbdff8
Eddie Kim <eekim125@yahoo.com>
2012-11-21 22:34:28 -0800

    added suggest poking on python version

--- nuclearpokebot.py
+++ nuclearpokebot.py
@@ -5,7 +5,7 @@ import cStringIO
 import re
 import time
 
-username = "email"
+username = "username"
 password = "password"
 
 def login():
@@ -58,7 +58,7 @@ def login():
 
 
 
-def pokeback():
+def poke():
     #enter infinite poke loop
     while True:
         buf = cStringIO.StringIO()
@@ -74,14 +74,37 @@ def pokeback():
         curlData = buf.getvalue()
         buf.close()
         #print curlData
-        pokedata = re.findall(ur"<div class=\"pokeHeader fsl fwb fcb\"><a href=\"(.*?)\" data-hovercard=\"\/ajax\/hovercard\/user.php\?id=([0-9]*)\">([^<]*)<\/a> has poked you.<\/div>",curlData)
-        #print pokedata
+        pokebackdata = re.findall(ur"<div class=\"pokeHeader fsl fwb fcb\"><a href=\"(.*?)\" data-hovercard=\"\/ajax\/hovercard\/user.php\?id=([0-9]*)\">([^<]*)<\/a> has poked you.<\/div>",curlData)
+        pokesuggestdata = re.findall(ur"<div class=\"userSuggestionName\"><span class=\"fwb\"><a href=\"https:\/\/www.facebook.com\/(.*?)\" data-hovercard=\"\/ajax\/hovercard\/user.php\?id=([0-9]*)\">([^<]*)<\/a><\/div>",curlData)
+        #print pokebackdata
+        userid = re.findall(ur"\"user\":\"([0-9]*)\"",curlData)
+        fb_dtsg = re.findall(ur"name=\"fb_dtsg\" value=\"([^\"]*)",curlData)
 
-        if len(pokedata)>0:
-            userid = re.findall(ur"\"user\":\"([0-9]*)\"",curlData)
-            fb_dtsg = re.findall(ur"name=\"fb_dtsg\" value=\"([^\"]*)",curlData)
+        if len(pokesuggestdata):
+            for victim in pokesuggestdata:
+                victimid = victim[1]
+                postdata = '__a=1&nctr[_mod]=pagelet_pysp&suggestion=1&__user='+str(userid[0])+'&fb_dtsg='+fb_dtsg[0]+'&uid='+str(victimid)
+                buf = cStringIO.StringIO()
+                c = pycurl.Curl()
+                c.setopt(c.URL, "https://www.facebook.com/ajax/pokes/poke_inline.php")
+                c.setopt(pycurl.COOKIEFILE, "pycookie.txt")
+                c.setopt(pycurl.COOKIEJAR, "pycookie.txt")
+                c.setopt(pycurl.WRITEFUNCTION, buf.write)
+                c.setopt(pycurl.FOLLOWLOCATION, 1)
+                c.setopt(pycurl.ENCODING, "")
+                c.setopt(pycurl.SSL_VERIFYPEER, 0)
+                c.setopt(pycurl.USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6 (.NET CLR 3.5.30729)")
+                c.setopt(pycurl.POST, 1)
+                c.setopt(pycurl.POSTFIELDS, postdata)
+                c.setopt(pycurl.POSTFIELDSIZE, len(postdata))
+                #c.setopt(pycurl.VERBOSE, True)
+                c.perform()
+                curlData = buf.getvalue()
+                buf.close()
+                print "You poked "+victim[2]+"!\n"
 
-            for victim in pokedata:
+        if len(pokebackdata)>0:
+            for victim in pokebackdata:
                 victimid = victim[1]
                 postdata = '__a=1&nctr[_mod]=pagelet_pokes&pokeback=1&__user='+str(userid[0])+'&fb_dtsg='+fb_dtsg[0]+'&uid='+str(victimid)
                 buf = cStringIO.StringIO()
@@ -101,14 +124,15 @@ def pokeback():
                 c.perform()
                 curlData = buf.getvalue()
                 buf.close()
-                print "You poked "+victim[2]+"!\n"
+                print "You return poked "+victim[2]+"!\n"
+
         else:
             time.sleep(3)
 
 
 def main():
     login()
-    pokeback()
+    poke()
 
 if __name__ == "__main__":
     main()
